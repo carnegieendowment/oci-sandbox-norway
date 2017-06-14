e1beb24cf5e61ab7840ac8d8cbb5426657f3c72a
sawojcicki <sawojcicki@gmail.com>
2017-06-14 10:12:00 -0400

    Create keys.js

--- test/keys.js
+++ test/keys.js
@@ -16,23 +16,28 @@ test('All JSON files should parse correctly', function (t) {
 test('All possible runs should be readable', function (t) {
   // Load all data based on metadata
   var metadata = JSON.parse(fs.readFileSync('../app/assets/data/metadata.json'));
-  var si = metadata.solarSteam.split(',');
+  var gi = metadata.gwp.split(',');
   var wi = metadata.water.split(',');
   var fi = metadata.flare.split(',');
   var ri = metadata.refinery.split(',');
   var li = [1, 0];
+  var yi = metadata.year.split(','); 
+  var zi = [1, 0];
 
-  si.forEach(function (_, s) {
+  gi.forEach(function (_, s) {
     wi.forEach(function (_, w) {
       fi.forEach(function (_, f) {
-        t.notThrows(function () { JSON.parse(fs.readFileSync('../app/assets/data/opgee/opgee_run' + s + w + f + '.json')); });
+        yi.forEach(function (_,y) {
+          t.notThrows(function () { JSON.parse(fs.readFileSync('../app/assets/data/opgee/opgee_run' + s + w + f + '.json')); });
+        });
       });
     });
   });
-
-  ri.forEach(function (_, r) {
-    li.forEach(function (_, l) {
-      t.notThrows(function () { JSON.parse(fs.readFileSync('../app/assets/data/prelim/prelim_run' + r + l + '.json')); });
+  zi.forEach(function (_,z) {
+    ri.forEach(function (_, r) {
+      li.forEach(function (_, l) {
+        t.notThrows(function () { JSON.parse(fs.readFileSync('../app/assets/data/prelim/prelim_run' + r + l + '.json')); });
+      });
     });
   });
 });
